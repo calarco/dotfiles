ff3452d1a59b4376604a6d334e6f637909204ddb
calarco <sebastiancalarco@gmail.com>
2014-06-01 22:26:44 +0000

    Updated scripts

--- home/.config/Brackets/extensions/user/terminus/main.js
+++ home/.config/Brackets/extensions/user/terminus/main.js
@@ -3,5 +3,5 @@ define(function (require, exports, module) {
 
     var ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
 
-    ExtensionUtils.addEmbeddedStyleSheet(".CodeMirror { font-family: 'Terminus (TTF)'; }");
+    ExtensionUtils.addEmbeddedStyleSheet(".CodeMirror { font-family: 'Envy Code R'; }");
 });
